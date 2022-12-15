f5083a882f6a1a6434a432109855c37d45bf45dc
codesche <110509654+codesche@users.noreply.github.com>
2022-12-15 11:55:49 +0900

    Time: 2 ms (88.26%), Space: 40.3 MB (87.50%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = Map.of(')', '(', '}', '{', ']', '[');
+		Stack<Character> stack = new Stack<>();
+
+		for (Character c : s.toCharArray()) {
+			if (c == '(' || c == '{' || c == '[') {
+				stack.push(c);
+			} else if (stack.isEmpty() || stack.pop() != map.get(c)) {
+				return false;
+			}
+		}
+        return stack.isEmpty();
+    }
+}
